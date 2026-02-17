e2acf88a5da365fd93d65dcd88a172ed13b6f3c9
n-graden <ngraden@umich.edu>
2026-02-17 15:31:50 -0500

    Second task done

--- discussion6.py
+++ discussion6.py
@@ -90,7 +90,20 @@ class HorseRaces:
             tuple of fastest race name and the time
             EXAMPLE: ('Teio Sho', 14.8)
         '''
-        pass
+        if horse not in self.race_dict:
+            return (None, 999.9)
+
+        horse_data = self.race_dict[horse]
+        
+        fastest_race = None
+        fastest_time = 999.9 
+
+        for race, time in horse_data.items():
+            if time < fastest_time:
+                fastest_time = time
+                fastest_race = race
+
+        return (fastest_race, fastest_time)
 
 ###############################################################################
 ##### TASK 3
